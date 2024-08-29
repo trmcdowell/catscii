4dc4d78798446feaf7a8ea6ddcd50fa1c3b17b02
Thomas McDowell <tmcdowell.dev@gmail.com>
2024-08-29 10:01:52 -0500

    feat: Added better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
+use color_eyre::eyre::eyre;
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let response = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
+    let url = get_cat_image_url().await.unwrap();
+    println!("The cat image is at {url}")
+}
+
+async fn get_cat_image_url() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let response = reqwest::get(api_url).await.unwrap();
     if !response.status().is_success() {
-        panic!("Request failed with HTTP status {}", response.status());
+        return Err(eyre!(
+            "Request failed with HTTP status {}",
+            response.status()
+        ));
     }
 
-    println!("Status: {}", response.status());
-    let images: Vec<CatImage> = response.json().await.unwrap();
-    let image = images
-        .first()
-        .expect("Cat API should return at least one image");
-    println!("The image is at {}", image.url);
+    let mut images: Vec<CatImage> = response.json().await?;
+
+    if let Some(image) = images.pop() {
+        Ok(image.url)
+    } else {
+        Err(eyre!("The Cat API returned no images"))
+    }
 }
 
 #[derive(Deserialize)]
