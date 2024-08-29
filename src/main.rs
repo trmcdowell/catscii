3c0687b93f014f418cabfefe137db56fffa65525
Thomas McDowell <tmcdowell.dev@gmail.com>
2024-08-29 09:44:50 -0500

    feat(CatImage): Created CatImage struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
     let response = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await
         .unwrap();
+    if !response.status().is_success() {
+        panic!("Request failed with HTTP status {}", response.status());
+    }
+
     println!("Status: {}", response.status());
-    let body = response.text().await.unwrap();
-    println!("Body: {}", body);
+    let images: Vec<CatImage> = response.json().await.unwrap();
+    let image = images
+        .first()
+        .expect("Cat API should return at least one image");
+    println!("The image is at {}", image.url);
+}
+
+#[derive(Deserialize)]
+struct CatImage {
+    url: String,
 }
