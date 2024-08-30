4db9d4af42977a14bca33a76d0e2a15b97daea5e
Thomas McDowell <tmcdowell.dev@gmail.com>
2024-08-30 11:20:06 -0500

    feat: Add axum and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-use color_eyre::eyre::eyre;
-use pretty_hex::PrettyHex;
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use tokio::net::TcpListener;
+
+use axum::{
+    body::Body,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use dotenv::dotenv;
+use reqwest::{header, StatusCode};
 use serde::Deserialize;
+use tracing::{error, info, Level};
+use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
-async fn main() {
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    // only dump the first 200 bytes so our terminal survives the
-    // onslaught. this will panic if the image has fewer than 200 bytes.
-    println!("{:?}", &image_bytes[..200].hex_dump());
+async fn main() -> anyhow::Result<()> {
+    // Build tracing subscriber
+    let filter = Targets::from_str(std::env::var("RUST_LOG").as_deref().unwrap_or("info"))
+        .expect("RUST_LOG should be a valid tracing filter");
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .json()
+        .finish()
+        .with(filter)
+        .init();
+
+    dotenv().ok();
+    let address = std::env::var("ADDRESS")?;
+    let app: Router = Router::new().route("/", get(root_get));
+    let listener = TcpListener::bind(address).await?;
+    info!("Listening on {:?}", listener.local_addr().unwrap());
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
 }
 
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn root_get() -> Response<Body> {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+        Err(e) => {
+            error!("Error getting ascii art: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Error getting ascii art").into_response()
+        }
+    }
+}
+
+async fn get_cat_ascii_art() -> anyhow::Result<String> {
     let api_url = "https://api.thecatapi.com/v1/images/search";
     let client = reqwest::Client::default();
 
@@ -22,16 +64,25 @@ async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
         .json::<Vec<CatImage>>()
         .await?
         .pop()
-        .ok_or_else(|| eyre!("The Cat API returned no images"))?;
+        .ok_or_else(|| anyhow!("The Cat API returned no images"))?;
+    println!("{}", image.url);
 
-    Ok(client
+    let image_bytes = client
         .get(image.url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
         .await?
-        .to_vec())
+        .to_vec();
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let artem_config = artem::config::ConfigBuilder::new()
+        .target(artem::config::TargetType::HtmlFile)
+        .build();
+    let ascii = artem::convert(image, &artem_config);
+
+    Ok(ascii)
 }
 
 #[derive(Deserialize)]
